6b5b9ae97690a9fbbc2e63986085143debdebd9d
badriahalmalki <badryahbalmalki@gmail.com>
2026-02-19 10:44:07 +0300

    implement core business logic classes and relationships

---
+++ part2/app/models/user.py
@@ -0,0 +1,53 @@
+import re
+from app.models import BaseModel
+
+EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
+
+
+class User(BaseModel):
+    def __init__(self, first_name, last_name, email, is_admin=False):
+        super().__init__()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.is_admin = is_admin
+
+    @property
+    def first_name(self):
+        return self._first_name
+
+    @first_name.setter
+    def first_name(self, value):
+        if not value or len(value) > 50:
+            raise ValueError(
+                "first_name is required and must be <= 50 characters")
+        self._first_name = value
+
+    @property
+    def last_name(self):
+        return self._last_name
+
+    @last_name.setter
+    def last_name(self, value):
+        if not value or len(value) > 50:
+            raise ValueError(
+                "last_name is required and must be <= 50 characters")
+        self._last_name = value
+
+    @property
+    def email(self):
+        return self._email
+
+    @email.setter
+    def email(self, value):
+        if not value or not EMAIL_REGEX.match(value):
+            raise ValueError("Invalid email format")
+        self._email = value
+
+    @property
+    def is_admin(self):
+        return self._is_admin
+
+    @is_admin.setter
+    def is_admin(self, value):
+        self._is_admin = bool(value)
